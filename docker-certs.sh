1a7cea34d2ac37d27e276022974f77b18bc85d36
Nils Nilsson <nilspontusnilsson@gmail.com>
2018-06-06 16:45:16 +0200

    Fixes to cert script

--- docker-certs.sh
+++ docker-certs.sh
@@ -2,7 +2,7 @@
 
 h=${1}
 # listen="-H unix:\/\/\/var\/run\/docker.sock -H tcp:\/\/0.0.0.0:2376"
-cert_location="\/var\/lib\/docker\/certs\/"
+cert_location="/var/lib/docker/certs/"
 default_options="--default-ulimit nofile=1024:4096"
 tls="--tlsverify --tlscacert=${cert_location}ca.crt --tlscert=${cert_location}${h}.crt --tlskey=${cert_location}${h}.key"
 
@@ -11,4 +11,5 @@ tls="--tlsverify --tlscacert=${cert_location}ca.crt --tlscert=${cert_location}${
 echo "[Service]
 Environment="DOCKER_OPTS=-H=0.0.0.0:2376 ${default_options} -H unix:///var/run/docker.sock ${tls}"" > /etc/systemd/system/docker.service.d/custom.conf
 
-sudo service docker restart
+sudo systemctl daemon-reload
+sudo systemctl restart docker
