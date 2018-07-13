cfae6569391c7e811621bc8bf2327954f948f121
Nils Nilsson <nilspontusnilsson@gmail.com>
2018-07-13 14:19:35 +0200

    Quote problem

--- docker-certs.sh
+++ docker-certs.sh
@@ -8,7 +8,7 @@ mkdir /etc/systemd/system/docker.service.d
 
 echo "
 [Service]
-Environment="DOCKER_OPTS=-H=0.0.0.0:2376 -H unix:///var/run/docker.sock ${tls}"
+Environment=\"DOCKER_OPTS=-H=0.0.0.0:2376 -H unix:///var/run/docker.sock ${tls}\"
 " >> /etc/systemd/system/docker.service.d/custom.conf
 
 sudo systemctl daemon-reload
