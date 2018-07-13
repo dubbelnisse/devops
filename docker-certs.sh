99a5dd5d0090e3c56af1d9a10aa07667460fefe2
Nils Nilsson <nilspontusnilsson@gmail.com>
2018-07-13 13:54:04 +0200

    Create folder before creating file

--- docker-certs.sh
+++ docker-certs.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 h=${1}
-# listen="-H unix:\/\/\/var\/run\/docker.sock -H tcp:\/\/0.0.0.0:2376"
 cert_location="/var/lib/docker/certs/"
-default_options="--default-ulimit nofile=1024:4096"
 tls="--tlsverify --tlscacert=${cert_location}ca.crt --tlscert=${cert_location}${h}.crt --tlskey=${cert_location}${h}.key"
 
-# sudo sed -i "s/^OPTIONS=.*/OPTIONS=\"${default_options} ${listen} ${tls}\"/" /etc/systemd/system/docker.service.d/custom.conf
+mkdir /etc/systemd/system/docker.service.d
 
-echo "[Service]
-Environment="DOCKER_OPTS=-H=0.0.0.0:2376 ${default_options} -H unix:///var/run/docker.sock ${tls}"" > /etc/systemd/system/docker.service.d/custom.conf
+echo "
+[Service]
+Environment="DOCKER_OPTS=-H=0.0.0.0:2376 -H unix:///var/run/docker.sock ${tls}"
+" >> /etc/systemd/system/docker.service.d/custom.conf
 
 sudo systemctl daemon-reload
 sudo systemctl restart docker
